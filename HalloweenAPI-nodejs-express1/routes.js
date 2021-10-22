de43f8b41942bc00e97df384e3d3415df21b8092
Likita <likitarai1@gmail.com>
2021-10-22 15:06:28 +0530

    Delete routes file

--- HalloweenAPI-nodejs-express1/routes.js
+++
@@ -1,8 +0,0 @@
-module.exports = function (app) {
-  /*
-  * Routes
-  */
-  app.use('/contestants', require('./routes/contestants.route'));
-  app.use('/root', require('./routes/root.route'));
-
-};
